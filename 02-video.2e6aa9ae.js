58895fe3dd8f07677a4330406c7a56fff16793b7
SzymonSliwa <114831810+SzymonSliwa@users.noreply.github.com>
2023-03-19 12:16:13 +0000

    Deploying to gh-pages from @ SzymonSliwa/goit-js-hw-08@1ab7bac9621b26163d45cf3d7bd6ddc44891acfe 🚀

--- 02-video.2e6aa9ae.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("timeupdate",(function(){console.log("played the video!"),o.getCurrentTime().then((function(e){console.log(e)}))})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.off("play");
-//# sourceMappingURL=02-video.2e6aa9ae.js.map
